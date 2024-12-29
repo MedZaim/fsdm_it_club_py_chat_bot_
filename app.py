b75e681402fe52bdca559753c4adf0e0551c6279
ZAIM Mohamed <mohamed.zaim3@uit.ac.ma>
2024-12-29 12:08:03 +0100

    Refactor API key and model name handling to use .env
    Introduced `.env` file for environment variables and updated `app.py` to load API key and model name securely using `dotenv` package. Removed hardcoded values to improve security and maintainability. Additional updates include auto-generated IDE configuration files to ensure proper project setup.

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import os
+
+import google.generativeai as genai
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-import google.generativeai as genai
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-import os
-from dotenv import load_dotenv
+from fastapi.templating import Jinja2Templates
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Read API Key and model name from environment variables
+API_KEY = os.getenv("API_KEY")
+MODEL_NAME = os.getenv("MODEL_NAME")
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -12,27 +20,27 @@ templates = Jinja2Templates(directory="templates")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust this to your needs
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/api/chatbot")
 async def say_hello(request: Request):
     data = await request.json()
     message = data.get("message")
 
-    API_KEY = os.getenv("API_KEY")
-    MODEL_NAME = os.getenv("MODEL_NAME")
 
-    genai.configure(api_key=os.getenv("API_KEY"))
-    model = genai.GenerativeModel(os.getenv("MODEL_NAME"))
-
+    #Configure API key   &   Use model name from environment variables
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel(MODEL_NAME)
 
     response = model.generate_content(message)
 
